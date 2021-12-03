64469ee06fa5947fcc6ecfc99d33d8287565a829
Will <willtheguvnor@gmail.com>
2021-12-03 19:40:03 +0000

    3-1

--- src/App/Diagnostic.fs
+++ src/App/Diagnostic.fs
@@ -1,3 +1,4 @@
+module Diagnostic
 
 let getRate (zeroes:int) (ones:int) (func:int->int->bool) =
     match (zeroes, ones) with
@@ -17,47 +18,25 @@ let generateRateValues (input:list<string>) (func:int->int->bool) (index:int) =
         |> List.filter (fun x -> x = '1')
         |> List.length
 
-    printfn "zeroes: %i" zeroes
-    printfn "ones: %i" ones
-
     getRate zeroes ones func
 
-let convertBinaryToInt (bin:list<int>) =
-    let inter = 
-        bin 
+let convertBinaryToDec (bin:list<int>) =
+    bin 
         |> List.rev
-    printfn "reversed: %A" inter
-
-    let inter2 =
-        inter
         |> List.mapi (fun i x -> if x = 0 then 0.0 else (2.0 ** (float i)))
-    printfn "to binary: %A" inter2
-    
-    inter2 |> List.sum
-
-let getPowerConsumption ints =
-    0
+        |> List.sum
 
 let analyseDiagnosticReport (diagnosticReport:seq<string>) =
     let rptList = diagnosticReport |> List.ofSeq
-    let passes = [0..(rptList.Head.Length)-1]
-    
-    printfn "Passes: %A" passes
+    let passes = [0..(rptList.Head.Length-1)]
 
     let gammaRate = 
         passes 
         |> List.map (generateRateValues rptList (<))
-    printfn "gammaRate: %A" gammaRate
 
     let epsilonRate =
         passes 
         |> List.map (generateRateValues rptList (>))
-    printfn "epsilonRate: %A" epsilonRate
 
-    printfn "gamma: %f epsilon: %f" (convertBinaryToInt gammaRate) (convertBinaryToInt epsilonRate)
-    (convertBinaryToInt gammaRate) * (convertBinaryToInt epsilonRate)
+    (convertBinaryToDec gammaRate) * (convertBinaryToDec epsilonRate)
 
-let inputs = [ "00001"; "10010"; "10100"; "01001"; "10000"; "11111" ]
-
-
-analyseDiagnosticReport inputs
--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,10 +1,12 @@
 ﻿open Depth
+open Diagnostic
 open Drive
 
 [<EntryPoint>]
 let main args =
     // printfn "%i" (countIncreases (Lib.readLines args[0]))
     // printfn "%i" (count3rdIncreases (Lib.readLines args[0]))
-    printfn "%i" (followInstructions (Lib.readLines args[0]))
-
+    // printfn "%i" (followInstructions (Lib.readLines args[0]))
+    printfn "%f" (analyseDiagnosticReport (Lib.readLines args[0]))
+    
     0 // return an integer exit code
